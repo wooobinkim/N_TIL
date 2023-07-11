1d3bcc24ffb3c0a5a7753198dd73e5143bca3d89
김우빈 <rladnqls98@gmail.com>
2023-07-11 22:33:25 +0900

    230711 TIL

--- SpringStudy/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringStudy/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,10 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    //서비스단에서 직접 바꿔야함.... 이딴게 SOLID...? 이딴게 OCP...? 이딴게 DIP...?
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order creatOrder(Long memberId, String itemName, int itemPrice) {
